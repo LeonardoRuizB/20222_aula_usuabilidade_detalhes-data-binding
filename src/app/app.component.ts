27c32c141e293bb2b7069d7f57b179b50cb4cc89
LeonardoRuizB <leobottura12@gmail.com>
2022-09-22 21:46:10 -0300

    Apostila03: Chegamos ao final da aula!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'detalhes-data-binding';
+
+  nome
+  esconderCaixa = true
+  numero
+
+  escolher(){
+    this.numero = Math.floor(Math.random() * 100) + 1
+  }
+
+  alterarNome(nome){
+    console.log(nome.target.value)
+    this.nome = nome.target.value
+  }
+  adicionar(nomeInput){
+    this.nome = nomeInput.value
+    this.esconderCaixa = nomeInput.value.length <= 0
+    // console.log(nomeInput.value)
+    //console.log("Adicionando...")
+  }
+
+  // nome = "Leonardo"
+  // idade = 20
+
+  // lancarDado(){
+  //   return Math.floor(Math.random() * 6) + 1
+  // }
 }
